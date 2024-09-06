6ebbebb4057db929e0c8b3253324637116e1399b
yasminkeche <128612457+yasminkeche@users.noreply.github.com>
2024-09-06 11:55:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultado");
+const caixaResultados = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    "Pergunta 1",
+    "Pergunta 2"
+];
+const perguntas = [
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+
+    "Pergunta 2"
+];
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+    "Pergunta 2"
+]; 
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
+]
